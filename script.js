21dc63e8423665f1377198100d6dc619da054e70
Ashwani Verma <89683890+Ashwani-Verma-07@users.noreply.github.com>
2022-04-04 13:10:30 +0530

    New UI

--- script.js
+++ script.js
@@ -307,6 +307,50 @@ document.addEventListener('DOMContentLoaded', () =>  {
 
 var myTimer = setInterval(addColours, 50)
 
+document.addEventListener('.grid', handleTouchStart, false);        
+document.addEventListener('.grid', handleTouchMove, false);
 
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+           keyRight()
+        } else {
+           keyLeft()
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            keyDown()
+        } else { 
+           keyUp()
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
 
 })
